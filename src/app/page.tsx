2382972b98b3d5ba808615afe651a4a93cf29e25
Monique <skaiwilliams85@gmail.com>
2023-09-06 23:15:17 -0500

    added content to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import Container from "@/components/Container";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>hey</main>;
+  return (
+    <main className="bg-[url('/space.png')] bg-no-repeat bg-center bg-cover h-screen -z-20">
+      <div className="bg-gradient-to-r from-violet-950 to-indigo-950 h-screen opacity-25 z-0 fixed top-0 right-0 w-full"></div>
+
+      <Container className="h-screen flex flex-col justify-center items-center gap-8">
+        <Image
+          src="/logo.png"
+          alt="final space logo"
+          width={500}
+          height={500}
+          className=""
+        />
+        <Link
+          href="/"
+          className="text-xl opacity-100 z-20 bg-indigo-300 text-indigo-600 rounded-md font-bold py-2 px-4"
+        >
+          Browse
+        </Link>
+      </Container>
+    </main>
+  );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Container = (props: Props) => {
+  const { children, className } = props;
+  return (
+    <div className={`w-[92%] my-0 mx-auto  ${className ?? ""} md:w-[90%]`}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
