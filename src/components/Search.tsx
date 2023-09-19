e1ff0ea08069305f4e0876109078bcdda7b233e5
Monique <skaiwilliams85@gmail.com>
2023-09-18 21:43:24 -0500

    set search params

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,28 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+
+import { ChangeEvent, useState } from "react";
 
 const Search = () => {
   const pathname = usePathname().split("/")[1];
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    router.push(`/${pathname}?search=${e.target.value}`);
+  };
+
   return (
     <form className="relative w-full">
       <input
         type="text"
         placeholder={`Search ${pathname}`}
-        className="rounded-md border border-slate-400 text-slate-400 placeholder:text-slate-400 py-[0.3rem] px-2 placeholder:text-sm placeholder:font-light focus:outline-none focus:ring-1 focus:ring-slate-400 focus:border-transparent w-full font-light text-sm bg-transparent"
+        className="search"
+        value={search}
+        onChange={handleSearch}
       />
       <MagnifyingGlassIcon className="absolute h-4 w-4 text-slate-400 top-2 right-2 " />
     </form>
