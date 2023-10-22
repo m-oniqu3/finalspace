6c7f2fe7e750439bb3e5e314490c88dae9a863ab
Monique <skaiwilliams85@gmail.com>
2023-10-22 18:49:00 -0500

    redirect user from /account

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,79 +1,14 @@
-"use client";
-
-import FormError from "@/app/account/FormError";
-import { validateEmail, validatePassword } from "@/app/account/validateForm";
+import Form from "@/app/account/Form";
 import Container from "@/components/ui/Container";
-import { createUser, signInUser } from "@/utils/auth";
-import { notify } from "@/utils/notify";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { Fragment, useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
-const Account = () => {
-  const router = useRouter();
-  const [showLoginForm, setShowLoginForm] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formErrors, setFormErrors] = useState({
-    email: { error: "", valid: false },
-    password: { error: "", valid: false },
-  });
-
-  const [isFormValid, setIsFormValid] = useState(false);
-
-  const formState = {
-    heading: showLoginForm ? "Sign In" : "Create Account",
-    text: showLoginForm
-      ? "New to Final Space? Get started by creating an account."
-      : "Welcome to Final Space. Already have an account? ",
-    link: showLoginForm ? "Create Account" : "Sign in",
-    button: showLoginForm ? "Sign In" : "Create Account",
-  };
-
-  const { heading, text, link, button } = formState;
-
-  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-
-    const results = validateEmail(e.target.value);
-    setFormErrors((state) => ({ ...state, email: results }));
-  };
-
-  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-
-    const results = validatePassword(e.target.value);
-    setFormErrors((state) => ({ ...state, password: results }));
-  };
-
-  useEffect(() => {
-    const { email, password } = formErrors;
-    const { valid: emailValid } = email;
-    const { valid: passwordValid } = password;
-
-    if (emailValid && passwordValid) {
-      setIsFormValid(true);
-    } else {
-      setIsFormValid(false);
-    }
-  }, [formErrors]);
-
-  const handleAccount = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    try {
-      if (showLoginForm) {
-        signInUser(email, password, router);
-      } else {
-        createUser(email, password, router);
-      }
-    } catch (error: any) {
-      console.log(error);
-      notify.error(error);
-    }
-  };
-
-  const handleChangeForm = () => setShowLoginForm((state) => !state);
+const Account = async () => {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+  if (data.session) redirect("/characters");
 
   return (
     <div className="bg-indigo-100 h-screen">
@@ -88,56 +23,7 @@ const Account = () => {
           />
         </Container>
       </nav>
-
-      <Container className="flex h-[80vh] justify-center items-center">
-        <form className="w-[90%] flex flex-col gap-2 max-w-xs">
-          <article className="py-4">
-            <h1 className="font-bold text-3xl text-indigo-900 mb-2">
-              {heading}
-            </h1>
-
-            <p className=" text-indigo-900">
-              {text} &nbsp;
-              <span
-                className="hover:underline cursor-pointer font-normal"
-                onClick={handleChangeForm}
-              >
-                {link}
-              </span>
-            </p>
-          </article>
-
-          <Fragment>
-            <input
-              type="email"
-              placeholder="Email"
-              className="input placeholder:text-indigo-900 text-indigo-900 w-full"
-              value={email}
-              onChange={handleEmail}
-            />
-            <FormError error={formErrors.email} />
-          </Fragment>
-
-          <Fragment>
-            <input
-              type="password"
-              placeholder="Password"
-              className="input placeholder:text-indigo-900 text-indigo-900 w-full"
-              value={password}
-              onChange={handlePassword}
-            />
-            <FormError error={formErrors.password} />
-          </Fragment>
-
-          <button
-            onClick={handleAccount}
-            className="submit disabled:opacity-30 disabled:cursor-not-allowed"
-            disabled={!isFormValid}
-          >
-            {button}
-          </button>
-        </form>
-      </Container>
+      <Form />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Container from "@/components/ui/Container";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Russo_One } from "next/font/google";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 
 const russo = Russo_One({ weight: "400", subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  let route = "/account";
+  if (data.session) route = "/characters";
+
   return (
     <main className="bg-[url('/space.png')] bg-no-repeat bg-center bg-cover h-screen -z-20">
       <div className="bg-gradient-to-r from-violet-950 to-indigo-950 h-screen opacity-25 z-0 fixed top-0 right-0 w-full"></div>
@@ -19,7 +27,7 @@ export default function Home() {
           className=""
         />
         <Link
-          href="/account"
+          href={route}
           className={`${russo.className} tracking-widest text-xl opacity-100 z-20 bg-indigo-300 text-indigo-600 rounded-md font-bold py-2 px-4 hover:bg-indigo-600 hover:text-indigo-300 transition duration-300 ease-in-out animate-bounce`}
         >
           Browse
