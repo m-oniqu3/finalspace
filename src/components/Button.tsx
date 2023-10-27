e76e5835d4c6cec8d631a75c1fb0b06caabbb186
Monique <skaiwilliams85@gmail.com>
2023-10-26 23:03:16 -0500

    added logout logic to mobile menu

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,29 @@
+import { Database } from "@/lib/database.types";
+import { notify } from "@/utils/notify";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
 const Button = () => {
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    supabase.auth
+      .signOut()
+      .then(() => {
+        router.push("/");
+        router.refresh();
+      })
+      .catch((error: any) => {
+        console.log(error);
+        notify.error(error.message);
+      });
+  };
   return (
-    <button className="font-light rounded-md bg-indigo-200 py-1.5 px-5 border border-indigo-300 min-w-fit hover:border-indigo-500 hover:bg-indigo-300  transition duration-300 ease-in-out">
+    <button
+      onClick={handleSignOut}
+      className="font-light rounded-md bg-indigo-200 py-1.5 px-5 border border-indigo-300 min-w-fit hover:border-indigo-500 hover:bg-indigo-300  transition duration-300 ease-in-out"
+    >
       Logout
     </button>
   );
